6637d2ecba747a03e570bfe6ad6fb08347ee3474
Saidkodirov <stolibjon123@gmail.com>
2024-07-03 13:14:25 +0500

    keybr

--- keyboards/default/channels_keyb.py
+++ keyboards/default/channels_keyb.py
@@ -3,13 +3,15 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 new_channel_or_delete = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton("✍🏻 Yangi kanal qo'shish")],
-        [KeyboardButton("🙅🏻‍♂️ Mavjud kanalni o'chirish")]
+        [KeyboardButton("🙅🏻‍♂️ Mavjud kanalni o'chirish")],
+        [KeyboardButton("🔙 Bosh sahifa")],
     ],
     resize_keyboard=True
 )
 new_channel_only = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton("✍🏻 Yangi kanal qo'shish")],
+        [KeyboardButton("🔙 Bosh sahifa")],
     ],
     resize_keyboard=True
 )
